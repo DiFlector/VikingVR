346c9045ae10da9aee714bdca6c83da800fe5340
DiFlector <egorgorbachenkov@gmail.com>
2023-04-06 23:58:21 +0300

    Good move

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class playerMove : MonoBehaviour
 {
     private CharacterController cc;
+    private Transform player;
     public float gravity = 0f;
     public float jumpForce = 0f;
     private float jspeed = 0f;
     public float speed = 0f;
 
+    public float horizontal = 0f;
+    public float vertical = 0f;
+
     private void Awake()
     {
         cc = GetComponent<CharacterController>();
+        player = GetComponent<Transform>();
     }
 
     private void Update()
     {
-        float horizontal = 0f;
-        float vertcal = 0f;
+        horizontal = 0f;
+        vertical = 0f;
+
+        if (Input.GetKey(KeyCode.D))
+            player.rotation *= Quaternion.Euler(0f, 1f * speed, 0f);
+        else if (Input.GetKey(KeyCode.A))
+            player.rotation *= Quaternion.Euler(0f, -1f * speed, 0f);
+
+        if (Input.GetKey(KeyCode.W))
+            vertical = 1f;
+        else if (Input.GetKey(KeyCode.S))
+            vertical = -1f;
 
         if (cc.isGrounded)
-            jspeed = 0f; 
+            jspeed = 0f;
         if (Input.GetKeyDown(KeyCode.Space) && jspeed == 0)
             jspeed = jumpForce;
-        
-        horizontal = Input.GetAxis("Horizontal");
-        vertcal = Input.GetAxis("Vertical");
+
         jspeed += gravity * Time.deltaTime * 3f;
-        Vector3 dir = new Vector3(horizontal * speed * Time.deltaTime, jspeed * Time.deltaTime, vertcal * speed * Time.deltaTime);
+
+        Vector2 moveDir = new Vector2(horizontal, vertical).normalized * speed * Time.deltaTime;
+        Vector3 dir = new Vector3(moveDir.x, jspeed * Time.deltaTime, moveDir.y);
         cc.Move(dir);
     }
 }
