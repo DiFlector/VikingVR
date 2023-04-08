3844eb0c6de0f905d867044656b2ecb31063d4b9
actuallynotsarcasm <timofeysimonov@mail.ru>
2023-04-08 23:21:57 +0300

    The Real Good Move

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -1,5 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private CharacterController cc;
+    private Animator animator;
+    public float gravity = 0f;
+    public float jumpForce = 0f;
+    private float jspeed = 0f;
+    public float speed = 0f;
+
+    private bool walk = false;
+
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontal = 0f;
+        float vertcal = 0f;
+
+        if (cc.isGrounded)
+            jspeed = 0f;
+        if (Input.GetKeyDown(KeyCode.Space) && jspeed == 0)
+        {
+            jspeed = jumpForce;
+            walk = false;
+            AnimationSet("Jump");
+        }
+
+        horizontal = Input.GetAxis("Horizontal");
+        vertcal = Input.GetAxis("Vertical");
+        jspeed += gravity * Time.deltaTime * 3f;
+        Vector3 dir = new Vector3(horizontal * speed * Time.deltaTime, jspeed * Time.deltaTime, vertcal * speed * Time.deltaTime);
+        cc.Move(dir);
+        if (horizontal != 0 || vertcal != 0)
+        {
+            AnimationSet("Walk");
+        }
+        else
+        {
+            AnimationSet("Idle");
+        }
+    }
+    private void AnimationSet(string anim)
+    {
+        animator.SetTrigger(anim);
+    }
+}
+
+
+/* using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Security.Cryptography;
 using System.Threading;
@@ -12,9 +68,10 @@ public class playerScript : MonoBehaviour
     private Animator animator;
 
     public float speed = 6;
-    public float gravity = -9.81f;
+    public float gravity = 0f;
     public float jumpHeight = 3;
-    Vector3 velocity;
+    public float jspeed = 0f;
+    public float jumpForce = 0f;
 
     float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
@@ -28,27 +85,23 @@ public class playerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-        if (controller.isGrounded)
-            velocity.y = -2f;
-
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-            walk = false;
-            AnimationSet("Jump");
-        }
-
-        //gravity
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
         //walk
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        //gravity
+        if (controller.isGrounded)
+            jspeed = 0f;
+        if (Input.GetKeyDown(KeyCode.Space) && jspeed == 0)
+        {
+            jspeed = jumpForce;
+            walk = false;
+            AnimationSet("Jump");
+        }
 
+            jspeed += gravity * Time.deltaTime * 3f;
+        direction += new Vector3(0f, jspeed * Time.deltaTime, 0f);
 
         if (direction.x != 0 || direction.z !=0)
         {
@@ -75,3 +128,4 @@ public class playerScript : MonoBehaviour
         animator.SetTrigger(anim);
     }
 }
+*/
